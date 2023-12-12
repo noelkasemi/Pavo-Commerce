cd16aa345afa5c30d7c5e349ca6028fb822dfde6
AleksanderDishnica <adishnica@yahoo.com>
2023-12-12 18:02:00 +0100

    Product details zoom fix

--- src/Page/ProductPage/MainImage.js
+++ src/Page/ProductPage/MainImage.js
@@ -1,36 +1,25 @@
 // MainImage.js
-import ReactImageMagnify from "react-image-magnify";
+import ImageMagnifier from "../../Tools/ImageMagnifier"
 const MainImage = ({ isMobile, handleMainImageClick, images, activeImageIndex, product }) => {
-    return (
-      <>
-        {isMobile ? (
-          <img
-            title={product.title}
-            onClick={() => handleMainImageClick()}
-            id="main-image"
-            className="w-2/4 p-4 cursor-pointer"
-            src={images[activeImageIndex]}
-            alt={product.title}
-          />
-        ) : (
-          <ReactImageMagnify
-            {...{
-              smallImage: {
-                alt: "",
-                isFluidWidth: true,
-                src: images[activeImageIndex],
-              },
-              largeImage: {
-                src: images[activeImageIndex],
-                width: 1200,
-                height: 800,
-              },
-            }}
-          />
-        )}
-      </>
-    );
-  };
-  
-  export default MainImage;
-  
+  return (
+    <>
+      {isMobile ? (
+        <img
+          title={product.title}
+          onClick={() => handleMainImageClick()}
+          id="main-image"
+          className="w-2/4 p-4 cursor-pointer"
+          src={images[activeImageIndex]}
+          alt={product.title}
+        />
+      ) : (
+        <ImageMagnifier
+          width={350}
+          src={images[activeImageIndex]}
+        />
+      )}
+    </>
+  );
+};
+
+export default MainImage;
